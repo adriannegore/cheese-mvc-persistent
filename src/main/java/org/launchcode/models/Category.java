e7e7535f7fae0c72df6d099e58ae705b27108376
Adrianne Gore <adrigore89@gmail.com>
2017-12-21 10:46:13 -0600

    end of part 2

--- src/main/java/org/launchcode/models/Category.java
+++ src/main/java/org/launchcode/models/Category.java
@@ -1,11 +1,11 @@
 package org.launchcode.models;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Category {
@@ -19,12 +19,15 @@ public class Category {
     @Size(min=3, max=15)
     private String name;
 
-    //Constructors
-    public Category() {};
+    @OneToMany
+    @JoinColumn(name="category_id")
+    private List<Cheese> cheeses = new ArrayList<>();
 
-    public Category(String name) {
-        this.name = name;
-    }
+
+    //Constructors
+    public Category() {}
+
+    public Category(String name) {this.name = name; }
 
     //Getters and Setters
     public int getId() {return id;}
@@ -33,8 +36,9 @@ public class Category {
 
     public void setName(String name) {this.name = name;}
 
+    public List<Cheese> getCheeses() {return cheeses;}
 
-
-
-
+    public String toString(){
+        return name;
+    }
 }
